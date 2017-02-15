0715ae4fddc054ae54a2abd7317710155af82590
pela3918 <alejandroluna_272@hotmail.com>
2017-02-15 16:29:41 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,11 +1,29 @@
 function Mostrar()
 {
-//tomo la edad  
+//tomo la edad  a para el invierno.Ya pasamos el frio, ahora calor!!!
 var mesDelAño = document.getElementById('mes').value;
+		switch(mesDelAño)
+		{
+			case "Julio":
+			case "Agosto":
+			alert("abrigate hace frio");
+			break;
+			case "Enero":
+			case "Febrero":
+			case "Marzo":
+			case "Abril":
+			case "Mayo":
+			case "Junio":
+			alert("Falta para el invierno");
+			break;	
+			case "Septiembre":
+			case "Octubre":
+			case "Noviembre":
+			case "Diciembre":
+			alert("Ya pasamos el frio, ahora calor!!!");
+			break;	  
 
-alert (mesDelAño);
-
-
+		}
 
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -3,10 +3,21 @@ function Mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
-	
-	
+	switch(mesDelAño)
+	{
+		case "Febrero":
+		alert("tiene 28");
+		break;
+		case "Abril":
+		case "Junio":
+		case "Septiembre":
+		case "Noviembre":
+		alert("tiene 30");
+		break;
+		default:
+		alert("tiene 31");
 
+	}
 
 
 }//FIN DE LA FUNCIÓN
