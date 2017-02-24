754ab98a808f6a5a3cc73e52879902d3138ae601
pela3918 <alejandroluna_272@hotmail.com>
2017-02-23 21:19:00 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,4 +1,90 @@
 function Mostrar()
 {
+	var contador=0 ;
+	var contador1=0 ;
+	var contador2=0 ;
+	var contador3=0 ;
+	var contador4=0 ;
+	var contador5=0 ;
+	var contador6=0 ;
+	var contador7=0 ;
+	var contador8=0 ;
+	var contador9=0 ;
+	var contador10=0 ;
+
+
+
+
+
+	var numero;
+	for(;contador<100000;contador++)
+
+	{
+		
+		var numero=Math.floor((Math.random() * 10) + 1);
+		
+		switch(numero)
+		{
+			case 1:
+			contador1++;
+			break;
+			case 2 :
+			contador2++;
+			break;
+			case 3 :
+			contador3++;
+			break;
+			case 4:
+			contador4++;
+			break;
+			case 5 :
+			contador5++;
+			break;
+			case 6 :
+			contador6++;
+			break;
+			case 7:			
+			if(contador7==7)
+			{
+				continue;
+			}
+			contador7++;
+			break;	
+			case 8 :
+			contador8++;
+			break;
+			case 9 :
+			contador9++;
+			break;
+			case 10 :
+			contador10++;
+			break;
+
+		}
+		contador++;
+
+		
+	
+		
+	}	
+	console.log("la cantidad de 1 : "+((contador1/contador)*100)+"%");
+	console.log("la cantidad de 2 : "+((contador2/contador)*100)+"%");
+	console.log("la cantidad de 3 : "+((contador3/contador)*100)+"%");
+	console.log("la cantidad de 4 : "+((contador4/contador)*100)+"%");
+	console.log("la cantidad de 5 : "+((contador5/contador)*100)+"%");
+	console.log("la cantidad de 6 : "+((contador6/contador)*100)+"%");
+	console.log("la cantidad de 7 : "+((contador7/contador)*100)+"%");
+	console.log("la cantidad de 8 : "+((contador8/contador)*100)+"%");
+	console.log("la cantidad de 9 : "+((contador9/contador)*100)+"%");
+	console.log("la cantidad de 10 : "+((contador10/contador)*100)+"%");
+	console.log(contador);
+
+
+
+
+
+
+
+
 
 }
--- 6-InstruccionFor/jsIteracionesFor (2).js
+++ 6-InstruccionFor/jsIteracionesFor (2).js
@@ -1,5 +1,12 @@
 function Mostrar()
 {
+	var contador ;
+	for(contador=10;contador<10;contador=contador-1)
+	{
+		alert("el numero es :"+contador);
+	}	
+
+
 
 
 }
