0f5706c5857425d24319b9744a939385907b0bbb
pela3918 <alejandroluna_272@hotmail.com>
2017-03-20 22:12:15 -0300

    Add files via upload

---
+++ alejandroluna/ejemplo1c/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+
+{
+   int numero1 ;
+   int numero2;
+   int resultado;
+   printf("ingrese un numero1");
+   scanf("%d",&numero1);
+   printf("ingrese un numero2");
+   scanf("%d",&numero2);
+   resultado=numero1+numero2;
+   printf("/n el resultado de los numeros  es :%d",resultado);
+   return 0;
+}
